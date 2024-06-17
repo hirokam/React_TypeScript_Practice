80a9a4dd408648a34056a90a91d5058fd8a32d21
hirokam <h.kameyama0329@gmail.com>
2024-06-17 12:19:14 +0900

    firstcommit

---
+++ src/Text.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+type props = {
+  color: string;
+  fontSize: string;
+};
+
+export const Text: FC<props> = (props) => {
+  const { color, fontSize } = props;
+  return <p style={{ color, fontSize }}>テキストです</p>;
+};
